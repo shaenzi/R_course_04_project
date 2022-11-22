ecf1077d66e1af16d4d67e1146f2a6b12cdc9507
Sara Hänzi <sarahaenzi@gmail.com>
2022-11-22 21:13:14 +0100

    add plotting function for map

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -24,27 +24,30 @@ get_csv_from_link <- function(my_url){
 #' @noRd
 get_data <- function() {
   emergency_calls <- get_csv_from_link("https://data.stadt-zuerich.ch/dataset/sid_srz_einsatzstatistik/download/SRZ_einsatzstatistik_seit2010.csv") %>%
+    janitor::clean_names() %>%
     # some NA values are indicated by "-" and therefore the columns are read as char
     dplyr::mutate(dplyr::across(.cols = tidyselect::where(is.character),
                                 .fns = as.numeric)) %>%
     #convert year to integer - TBD/todo whether this is correct, first of January is odd, could also be the delivery date
     dplyr::mutate(jahr = lubridate::year(jahr))
-  ambulances <- get_csv_from_link("https://data.stadt-zuerich.ch/dataset/sid_srz_hilfsfirsten_rd/download/hilfsfrist_rd.csv")
-  fire_service <- get_csv_from_link("https://data.stadt-zuerich.ch/dataset/sid_srz_ausrueckzeiten_fw/download/ausrueckzeit_fw.csv")
+  ambulances <- get_csv_from_link("https://data.stadt-zuerich.ch/dataset/sid_srz_hilfsfirsten_rd/download/hilfsfrist_rd.csv") %>%
+    janitor::clean_names()
+  fire_service <- get_csv_from_link("https://data.stadt-zuerich.ch/dataset/sid_srz_ausrueckzeiten_fw/download/ausrueckzeit_fw.csv") %>%
+    janitor::clean_names()
 
   return(list("emergency_calls" = emergency_calls,
               "ambulance" = ambulances,
               "fire_service" = fire_service))
 }
 
-#' plot_calls
+#' plot_emergency_calls
 #'
 #' @param emergency_calls tibble on the emergency calls
 #' @param group_selector filters the dat to plot, can be fw, anz, san (?), rd (?)
 #'
 #' @return ggplot object
 #' @noRd
-plot_calls <- function(emergency_calls, group_selector) {
+plot_emergency_calls <- function(emergency_calls, group_selector) {
   emergency_calls %>%
     dplyr::select(jahr, dplyr::starts_with(group_selector)) %>%
     tidyr::pivot_longer(cols = dplyr::starts_with(group_selector)) %>%
@@ -59,6 +62,39 @@ plot_calls <- function(emergency_calls, group_selector) {
 #'
 #' @return ggplot object
 #' @noRd
-plot_response_times <- function(response_times) {
-  #todo
+plot_response_times <- function(response_times, year) {
+  response_times <- response_times %>%
+    dplyr::filter(jahr == year)
+
+  # prepare stuff for plotting: labels, quantiles etc.
+  quantile_vec <- response_times %>%
+    dplyr::pull(prozent_einsaetze_bis_10min) %>%
+    quantile(probs = seq(0, 1, 0.2), na.rm = TRUE)
+
+  labels <- dplyr::tibble(
+    lab1 = quantile_vec,
+    lab2 = c(quantile_vec[2:length(quantile_vec)], NA)) %>%
+    dplyr::slice(1:dplyr::n() - 1) %>% # We remove the last row, since it has no meaning
+    dplyr::mutate_all(round, digits = 0) %>% # We remove digits after the 0
+    dplyr::mutate(labs = paste(lab1, lab2, sep = " - ")) %>%
+    dplyr::mutate(labs = paste0(labs, "%")) %>%
+    dplyr::pull(labs)
+
+  # join the two tibbles and plot as a map
+  # always need to join with sf as main tibble, otherwise cannot plot
+  zurich_kreise %>%
+    dplyr::left_join(response_times, by = c("bezeichnun" = "stadtkreis")) %>%
+    dplyr::mutate(quantiles = cut(prozent_einsaetze_bis_10min,
+                                  breaks = quantile_vec,
+                                  labels = labels,
+                                  include.lowest = TRUE)) %>%
+    ggplot2::ggplot() +
+    ggplot2::geom_sf(ggplot2::aes(fill = quantiles),
+                     color = "white") +
+    ggplot2::scale_fill_viridis_d(option = "E") +
+    ggplot2::theme_void()
 }
+
+
+
+
--- data-raw/zurich_kreise.R
+++ data-raw/zurich_kreise.R
@@ -1,5 +1,5 @@
 # read the locally saved/raw shape files. These are available as open government data from https://www.stadt-zuerich.ch/geodaten/download/Stadtkreise?format=10007
 zurich_kreise <- sf::st_read("data-raw/stadtkreise/stzh.adm_stadtkreise_a.shp") %>%
-  dplyr::select(-objid, -entstehung)
+  dplyr::select(-objid, -entstehung, -name)
 
 usethis::use_data(zurich_kreise, overwrite = TRUE)
