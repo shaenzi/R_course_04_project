4e591a1ab53dc49c04ab754410dd0bdd4b850d96
Sara Hänzi <sarahaenzi@gmail.com>
2022-12-12 20:49:41 +0100

    add documentation fo zurich urban districts

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Urban districts ("Stadtkreise" ) of Zurich
+#'
+#' @description Geometries/polygons of the urban districts of Zurich, downloaded from the open government data portal
+#'
+#' @format ## `zurich_kreise`
+#' \describe{
+#'   \item{bezeichnun}{Name of the district, "Kreis 1" to "Kreis 12"}
+#'   \item{geometry}{polygon to be plotted, borders of the urban district}
+#'   }
+#'
+#' @source <https://www.stadt-zuerich.ch/geodaten/download/Stadtkreise?format=10007>
+#'
+"zurich_kreise"
